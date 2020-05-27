9d9e907b372ebfead833d07b3bb55bc0c947e89f
tmzkt21 <ehswn67@naver.com>
2020-05-27 13:10:38 +0900

    Initial commit

---
+++ src/frontend/src/store/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import crawling from "./crawling"
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+    modules : {
+        crawling
+    }
+})
+
+
